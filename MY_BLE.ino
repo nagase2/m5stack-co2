4df9c427921775312929b76ed04452938eff0077
yoshimasa nagase <nagase2@gmail.com>
2021-01-23 22:30:01 +0900

    init

---
+++ MY_BLE.ino
@@ -0,0 +1,17 @@
+#include <M5Stack.h>
+
+// the setup routine runs once when M5Stack starts up
+void setup(){
+
+  // Initialize the M5Stack object
+  M5.begin();
+
+  // LCD display
+  M5.Lcd.print("Hello World!¥n");
+  M5.Lcd.print("M5Stack is running successfully!");
+}
+
+// the loop routine runs over and over again forever
+void loop() {
+
+}
